c56a51f80b928408e22de5c934f791a29a4d0af1
Cyrille Otieno <cyrilleotieno@apples-MacBook-Pro-4.local>
2022-09-09 09:01:38 +0300

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:chekikeja/locator/locator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Firebase.initializeApp();
+  setupLocator();
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  }
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
